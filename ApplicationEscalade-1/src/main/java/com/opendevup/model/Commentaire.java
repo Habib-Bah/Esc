9b315651764d19aa157042a6336f2ea7c74d5920
Habib <thiernohabibbah@gmail.com>
2019-03-17 10:33:50 +0100

    Amelioration de la mise en page

--- ApplicationEscalade-1/src/main/java/com/opendevup/model/Commentaire.java
+++ ApplicationEscalade-1/src/main/java/com/opendevup/model/Commentaire.java
@@ -2,6 +2,8 @@ package com.opendevup.model;
 
 import java.io.*;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "commentaire", schema = "public")
@@ -15,9 +17,13 @@ public class Commentaire implements Serializable{
 	private long id;
 	
 	@Column(name="nomutilisateur")
+	@NotNull
+	@Size(min=2, max=10) 
 	private String nomutilisateur;
 	
 	@Column(name="texte")
+	@NotNull
+	@Size(min=2, max=100) 
 	private String texte;
 
 	public Commentaire() {
--- ApplicationEscalade-1/src/main/java/com/opendevup/web/UtilisateurController.java
+++ ApplicationEscalade-1/src/main/java/com/opendevup/web/UtilisateurController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.opendevup.dao.CommentaireRepository;
 import com.opendevup.dao.UtilisateurRepository;
 import com.opendevup.model.Commentaire;
 import com.opendevup.model.Reservation;
@@ -21,6 +22,9 @@ public class UtilisateurController {
 	
 	@Autowired
 	private UtilisateurRepository utilisateurrepository;
+	
+	@Autowired
+	private CommentaireRepository commentairerepository;
 	
 	@GetMapping("/greeting")
     public String greetingForm(Model model) {
@@ -33,6 +37,16 @@ public class UtilisateurController {
         return "result";
     }
 	
+	/*@RequestMapping(value="/saveCommentaire")
+	public String saveC(Model model, @Valid Commentaire c, BindingResult bindingresult) {
+		
+		if(bindingresult.hasErrors()) {
+			return"greeting";
+		}
+		
+		commentairerepository.save(c);
+		return "resultatcommentaire";
+	}*/
 	@RequestMapping(value="/inscription")
 	public String Inscriptionform(Model model) {
 		model.addAttribute("utilisateur", new Utilisateur());
