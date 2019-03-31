02819bd08a69796c9d53064b271b87ef388c757a
Habib <thiernohabibbah@gmail.com>
2019-03-31 11:00:51 +0200

    Ajout du module de fin de reservation

--- ApplicationEscalade-1/src/main/java/com/opendevup/web/MainController.java
+++ ApplicationEscalade-1/src/main/java/com/opendevup/web/MainController.java
@@ -26,25 +26,25 @@ import com.opendevup.utils.WebUtils;
 @Controller
 public class MainController {
 
-	static List <Topos> toposL;
-	
+	static List<Topos> toposL;
+
 	@Autowired
 	AppUserRepository appuserrepository;
-	
+
 	@Autowired
 	CommentaireRepository commentairerepository;
-	
+
 	@Autowired
 	ToposRepository toporepository;
-	
+
 	@Autowired
 	ToposRepository toposrepository;
-	
+
 	@Autowired
 	ReservationRepository reservationrepository;
-	
+
 	EncrytedPasswordUtils encrypteP;
-	
+
 	@RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
 	public String welcomePage(Model model) {
 		model.addAttribute("title", "Viens on grimpe");
@@ -78,7 +78,6 @@ public class MainController {
 	@RequestMapping(value = "/userInfo", method = RequestMethod.GET)
 	public String userInfo(Model model, Principal principal) {
 
-		
 		String userName = principal.getName();
 
 		System.out.println("Nom d'utilisateur: " + userName);
@@ -101,110 +100,136 @@ public class MainController {
 
 			model.addAttribute("userInfo", userInfo);
 
-			/*String message = "Hi " + principal.getName()
-					+ "<br> Vous n'etes pas autorisé à faire cette action!";
-			model.addAttribute("message", message);*/
+			/*
+			 * String message = "Hi " + principal.getName() +
+			 * "<br> Vous n'etes pas autorisé à faire cette action!";
+			 * model.addAttribute("message", message);
+			 */
 
 		}
 
 		return "403Page";
 	}
-	
 
-	@RequestMapping(value="/form", method = RequestMethod.GET)
+	@RequestMapping(value = "/form", method = RequestMethod.GET)
 	public String formUser(Model model) {
-		
+
 		model.addAttribute("appuser", new AppUser());
 		return "formUser";
 	}
-	
-	@RequestMapping(value="/save", method = RequestMethod.POST)
-	public String save(Model model,  AppUser u) {
-		
-		
-		/*if(bindingresult.hasErrors()) {
-			return"formUser";
-		}
-		
-		String password = u.getEncrytedPassword();
-		
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        u.setEncrytedPassword(encoder.encode(password));*/
-		
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Model model, AppUser u) {
+
+		/*
+		 * if(bindingresult.hasErrors()) { return"formUser"; }
+		 * 
+		 * String password = u.getEncrytedPassword();
+		 * 
+		 * BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		 * u.setEncrytedPassword(encoder.encode(password));
+		 */
+
 		appuserrepository.save(u);
 		return "confirmation";
 	}
-	
-	@RequestMapping(value="/formc", method = RequestMethod.GET)
-    public String greetingForm(Model model) {
-        model.addAttribute("commentaire", new Commentaire());
-        return "formC";
-    }
-	
-	@RequestMapping(value="/saveC",method = RequestMethod.POST)
-    public String greetingSubmit(Model model, Commentaire c) {
-		
+
+	@RequestMapping(value = "/formc", method = RequestMethod.GET)
+	public String greetingForm(Model model) {
+		model.addAttribute("commentaire", new Commentaire());
+		return "formC";
+	}
+
+	@RequestMapping(value = "/saveC", method = RequestMethod.POST)
+	public String greetingSubmit(Model model, Commentaire c) {
+
 		commentairerepository.save(c);
-        return "result";
-    }
-	
-	
+		return "result";
+	}
+
 	@RequestMapping(value = "/formR", method = RequestMethod.GET)
-	public String recherTopo(Model model, @RequestParam(name="localisation", defaultValue="")String mc) {
-		
+	public String recherTopo(Model model, @RequestParam(name = "localisation", defaultValue = "") String mc) {
+
 		toposL = toposrepository.findByLocalisation(mc);
 		model.addAttribute("toposL", toposL);
 		return "formR";
 	}
-	
-	@RequestMapping(value="/recherche",method = RequestMethod.POST)
-    public String reche(Model model) {
-		
+
+	@RequestMapping(value = "/recherche", method = RequestMethod.POST)
+	public String reche(Model model) {
+
 		model.addAttribute("toposL", toposL);
 		return "recherche";
-    }
-	
-	@RequestMapping(value="/formReservation", method = RequestMethod.GET)
+	}
+
+	@RequestMapping(value = "/formReservation", method = RequestMethod.GET)
 	public String formReservation(Model model) {
-		
+
 		model.addAttribute("reservation", new Reservation());
 		return "formReservation";
 	}
-	
-	@RequestMapping(value="/saveReservation", method = RequestMethod.POST)
-	public String saveR(Model model,  Reservation reservation) {
-		
-		List <Topos> t = toporepository.findAll();
-		
-		for( Topos topo : t) {
-			
-			if(topo.getNomsite().equalsIgnoreCase(reservation.getNomsite()) || !topo.getNomsite().equalsIgnoreCase(reservation.getNomsite())) {
-				if(topo.isDisponibilite()) {
+
+	@RequestMapping(value = "/saveReservation", method = RequestMethod.POST)
+	public String saveR(Model model, Reservation reservation) {
+
+		List<Topos> t = toporepository.findAll();
+
+		for (Topos topo : t) {
+
+			if (topo.getNomsite().equalsIgnoreCase(reservation.getNomsite())
+					|| !topo.getNomsite().equalsIgnoreCase(reservation.getNomsite())) {
+				if (topo.isDisponibilite()) {
 					topo.setDisponibilite(false);
-				}
-				else {
-					return"mauvaiseReservation";
+				} else {
+					return "mauvaiseReservation";
 				}
 			}
-			
+
 		}
 
 		reservationrepository.save(reservation);
 		return "resultatReservation";
 	}
-	
-	@RequestMapping(value="/formTopos", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/formTopos", method = RequestMethod.GET)
 	public String formTopo(Model model) {
-		
+
 		model.addAttribute("topos", new Topos());
 		return "formTopos";
 	}
-	
-	@RequestMapping(value="/saveTopo", method = RequestMethod.POST)
-	public String saveT(Model model,  Topos topos) {
-		
+
+	@RequestMapping(value = "/saveTopo", method = RequestMethod.POST)
+	public String saveT(Model model, Topos topos) {
+
 		topos.setDisponibilite(true);
 		toporepository.save(topos);
 		return "resultatTopos";
+	}
+
+	@RequestMapping(value = "/fin", method = RequestMethod.GET)
+	public String finReser(Model model) {
+
+		model.addAttribute("reservation", new Reservation());
+		return "fin";
+	}
+
+	@RequestMapping(value = "/saveFin", method = RequestMethod.POST)
+	public String saveFinRE(Model model, Reservation reservation) {
+
+		List<Topos> t = toporepository.findAll();
+		for (Topos topo : t) {
+
+			if (topo.getNomsite().equalsIgnoreCase(reservation.getNomsite())){
+				if (topo.isDisponibilite()) {
+					return "mauvaiseReservation2";
+					
+					
+				} else {
+					topo.setDisponibilite(true);
+				}
+			}
+
+		}
+		return "resultatFinReservation";
 	}
 }
